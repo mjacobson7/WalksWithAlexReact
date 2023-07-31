ddc24230fa89ddad3db1be32425cee30352775a8
Max Jacobson <mjacobson7@gmail.com>
2023-07-30 20:04:48 -0600

    update services images

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,16 @@
 import React from "react";
-import walk30Image from "../images/walk30.webp";
-import virtualTraining from "../images/services_virtual-training.webp";
-import inPersonTraining from "../images/services_in-person-training.webp";
-import trainingWalk from "../images/services_training-walk.webp";
-import virtualPlusInPerson from "../images/services_virtual-plus-in-person.webp";
-import daycareImage from "../images/daycare.png";
-import trainingImage from "../images/training.jpeg";
 import photographyImage from "../images/services_pet-photography.jpeg";
 import ServiceItem from "./ServiceItem";
+
+import addOnInPerson from "../images/add-on-in-person.webp";
+import daycamp from "../images/daycamp.webp";
+import groupClass from "../images/group-class.webp";
+import inPersonTraining from "../images/in-person-training.webp";
+import photography from "../images/photography.webp";
+import trainingWalk from "../images/training-walk.webp";
+import victor from "../images/victor.webp";
+import virtualTraining from "../images/virtual-training.webp";
+import walks from "../images/walks.webp";
 
 const Services = () => {
   return (
@@ -93,61 +96,11 @@ const Services = () => {
             ]}
             image={trainingWalk}
           />
-          {/* <ServiceItem
-            title="Virtual Training Assessment + 3 In-Person training Sessions"
-            price="$400"
-            visibleText={[
-              "Virtual Assessment",
-              [
-                "A pre-assessment questionnaire to learn more about you and your dog's goals",
-                "Review of video examples of your dog’s behavior",
-                "1 hour discussion and virtual training with your dog",
-                "A detailed training program customized specifically for your dog",
-              ],
-            ]}
-            hiddenText={[
-              "In-person",
-              [
-                "60-90 minute session",
-                "Assess your progress so far",
-                "Fine tune your custom training program",
-                "Intensive hands-on guidance to ensure you are able to enact your training program",
-              ],
-              "A digital folder containing handouts, videos, progress reports, and homework",
-              "6 weeks of continued support via phone/email",
-            ]}
-            image={virtualPlusInPerson}
-          /> */}
-          {/* <ServiceItem
-            title="Virtual Training Assessment + 5 In-Person training Sessions"
-            price="$600"
-            visibleText={[
-              "Virtual Assessment",
-              [
-                "A pre-assessment questionnaire to learn more about you and your dog's goals",
-                "Review of video examples of your dog’s behavior",
-                "1 hour discussion and virtual training with your dog",
-                "A detailed training program customized specifically for your dog",
-              ],
-            ]}
-            hiddenText={[
-              "In-person",
-              [
-                "60-90 minute session",
-                "Assess your progress so far",
-                "Fine tune your custom training program",
-                "Intensive hands-on guidance to ensure you are able to enact your training program",
-              ],
-              "A digital folder containing handouts, videos, progress reports, and homework",
-              "8 weeks of continued support via phone/email",
-            ]}
-            image={virtualPlusInPerson}
-          /> */}
           <ServiceItem
             title="Group Classes"
             price="#TBD"
             visibleText="Coming Soon!"
-            image={trainingImage}
+            image={groupClass}
           />
           <ServiceItem
             title="Walks"
@@ -155,7 +108,7 @@ const Services = () => {
             visibleText="A customized experience for your dog to keep them physically and"
             hiddenText={`mentally engaged. What's included: a recap of our adventure,
 								action photo, and an activity report. (Additional dog: $10)`}
-            image={walk30Image}
+            image={walks}
           />
           <ServiceItem
             title="Daycamp"
@@ -165,7 +118,7 @@ const Services = () => {
 						needs and comfort level, which includes opportunities for play time with humans and other
 						 dogs, mental and physical enrichment, and training using positive reinforcement and games.
 						  In addition you will receive a report of their experience, photos, and on demand updates.`}
-            image={daycareImage}
+            image={daycamp}
           />
           <ServiceItem
             title="Photography"
@@ -173,7 +126,7 @@ const Services = () => {
             visibleText="A custom photoshoot in the studio or at an outdoor location with
 						5-10"
             hiddenText="portraits of your pet to choose from."
-            image={photographyImage}
+            image={photography}
           />
         </ul>
       </section>
